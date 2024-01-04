6d7d5a7a1a53e5b62d768dc3a398afc36ff10ae4
gaurav srivastava <gaurav.srivastava@enine.school>
2024-01-04 09:05:37 +0530

    need to work on return statement

--- answer.py
+++ answer.py
@@ -1,81 +1,15 @@
-# hash_dict={1:'ONE',2:'TWO',3:'THREE',4:'FOUR',5:'FIVE',6:'SIX',7:'SEVEN',8:'EIGHT',9:'NINE',10:'TEN',11:'ELEVEN',12:'TWELVE',13:'THIRTEEN',14:'FOURTEEN',15:'FIFTEEN',
-#            16:'SIXTEEN',17:'SEVENTEEN',18:'EIGHTEEN',19:'NINETEEN',20:'TWENTY',30:'THIRTY',40:'FORTY',50:'FIFTY',60:'SIXTY',70:'SEVENTY',80:'EIGHTY',90:'NINTY',
-#            100:'HUNDRED',1000:'THOUSAND',10000:'TEN THOUSAND',100000:'HUNDRED THOUSAND',1000000:'MILLION'}
 
-# # x=1234
-# x=23455
-# x=str(x)
-# c=len(x)
-# # print(c)
-
-# result=''
-# if len(x) in hash_dict:
-#     # print('yes')
-#     for i in range(len(x)):
-#         d='0'*(c-1-i)
-#         final='1'+d
-#         # print(final)
-
-        
-        
-#         # print(hash_dict[int(x[i])])
-#         if i<3:
-#             result+=hash_dict[int(x[i])]+' '+hash_dict[int(final)]+' '
-# # print(x[-2:])
-# ss=x[-2]+'0'
-# # print(ss)
-# print(result)
-# # print(hash_dict[int(ss)])
-# result=result + hash_dict[int(ss)]+' ' +hash_dict[int(x[-1])]
-# print(result)
+'''convert num to its spelling'''
+from solver import solver
 
 
+def answer():
+    """all the numbers from 1 to 1000
+    (one thousand) inclusive were written out in words,
+    how many letters would be used"""
 
-# x = 1234
-
-# x = 9338
-# x = str(x)
-# c = len(x)
-# result = ''
-
-# for i in range(len(x)):
-#     d = '0' * (c - 1 - i)
-#     final = '1' + d
-#     if i < len(x)-2:
-#         result += hash_dict[int(x[i])] + ' ' + hash_dict[int(final)] + ' '
-
-# ss = x[-2] + '0'
-# result = result +'AND'+' '+ hash_dict[int(ss)] + ' ' + hash_dict[int(x[-1])]
-# print(result)
+    return solver(1, 1000)
 
 
-# hash_dict = {0:'ZERO',1: 'ONE', 2: 'TWO', 3: 'THREE', 4: 'FOUR', 5: 'FIVE', 6: 'SIX', 7: 'SEVEN', 8: 'EIGHT', 9: 'NINE', 10: 'TEN', 11: 'ELEVEN', 12: 'TWELVE', 13: 'THIRTEEN', 14: 'FOURTEEN', 15: 'FIFTEEN',
-# 16: 'SIXTEEN', 17: 'SEVENTEEN', 18: 'EIGHTEEN', 19: 'NINETEEN', 20: 'TWENTY', 30: 'THIRTY', 40: 'FORTY', 50: 'FIFTY', 60: 'SIXTY', 70: 'SEVENTY', 80: 'EIGHTY', 90: 'NINETY',
-# 100: 'HUNDRED', 1000: 'THOUSAND', 1000000: 'MILLION',1000000000:'BILLION'}
-
-# num=1234
-# print(1234//10)
-# num_str=str(num)
-# for i in num_str:
-#     if 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    print(answer())
